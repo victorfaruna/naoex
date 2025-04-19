6fe50deaa96ccdb0dbc959f869f17887652f21bb
victorfaruna <farunavictor043@gmail.com>
2025-04-19 22:15:56 +0100

    update

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -220,13 +220,19 @@ export default function About() {
                                 <ul className="social">
                                     <li className="corner-box">
                                         <a href="#">
-                                            <Image
-                                                src={Logo}
-                                                className="text-color-2"
-                                                alt=""
-                                                width="32"
-                                                height="32"
-                                            />
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                x="0px"
+                                                y="0px"
+                                                width="30"
+                                                height="30"
+                                                viewBox="0 0 50 50"
+                                            >
+                                                <path
+                                                    d="M 6.9199219 6 L 21.136719 26.726562 L 6.2285156 44 L 9.40625 44 L 22.544922 28.777344 L 32.986328 44 L 43 44 L 28.123047 22.3125 L 42.203125 6 L 39.027344 6 L 26.716797 20.261719 L 16.933594 6 L 6.9199219 6 z"
+                                                    fill="var(--color-2)"
+                                                ></path>
+                                            </svg>
                                         </a>
                                     </li>
                                     <li className="corner-box">
