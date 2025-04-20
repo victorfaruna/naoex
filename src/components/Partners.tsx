579b8140d567f2f55c57d8cbfc1d821afc0129e2
victorfaruna <farunavictor043@gmail.com>
2025-04-20 20:21:04 +0100

    update

--- src/components/Partners.tsx
+++ src/components/Partners.tsx
@@ -1,18 +1,26 @@
 "use client";
 import React from "react";
 import Image from "next/image";
-import { Swiper, SwiperSlide } from "swiper/react";
-
-// Import Swiper styles
-import "swiper/css";
 
 export default function Partners() {
+    const LOGOS = [
+        require("@/assets/images/logo/logo1.png"),
+        require("@/assets/images/logo/logo9.png"),
+        require("@/assets/images/logo/logo3.png"),
+        require("@/assets/images/logo/logo4.png"),
+        require("@/assets/images/logo/logo5.png"),
+        require("@/assets/images/logo/logo6.png"),
+
+        require("@/assets/images/logo/logo10.png"),
+        require("@/assets/images/logo/logo11.png"),
+        require("@/assets/images/logo/logo12.png"),
+    ];
     return (
         <section
             className="tf-section partner bg-st1 pb-mobie-40"
             id="partners"
         >
-            <div className="container">
+            <div className="container mb-[40px]">
                 <div className="row">
                     {/* Title */}
                     <div className="col-md-12">
@@ -24,185 +32,23 @@ export default function Partners() {
                             <p className="h8 sub-title">
                                 Compliance and Partners
                             </p>
-                            <h4 className="title">
+                            <h4 className="title mb-[30px]">
                                 Compliance and Partnerships
                             </h4>
                         </div>
                     </div>
 
                     {/* Partners Carousel */}
-                    <div className="col-md-12">
-                        <div
-                            className="item-parner"
-                            data-aos="fade-up"
-                            data-aos-duration="800"
-                        >
-                            {/* First Swiper Carousel */}
-                            <Swiper
-                                slidesPerView={2}
-                                loop={false}
-                                spaceBetween={30}
-                                breakpoints={{
-                                    0: {
-                                        slidesPerView: 2,
-                                        spaceBetween: 30,
-                                    },
-                                    400: {
-                                        slidesPerView: 2,
-                                        spaceBetween: 60,
-                                    },
-                                    600: {
-                                        slidesPerView: 3,
-                                        spaceBetween: 60,
-                                    },
-                                    750: {
-                                        slidesPerView: 4,
-                                        spaceBetween: 60,
-                                    },
-                                    991: {
-                                        slidesPerView: 5,
-                                        spaceBetween: 60,
-                                    },
-                                    1280: {
-                                        slidesPerView: 6,
-                                        spaceBetween: 60,
-                                    },
-                                }}
-                                className="swiper-container sl-parner"
-                            >
-                                <SwiperSlide>
-                                    <div className="image">
-                                        <Image
-                                            src={require("@/assets/images/logo/logo1.png")}
-                                            alt="Logo 1"
-                                        />
-                                    </div>
-                                </SwiperSlide>
-                                <SwiperSlide>
-                                    <div className="image">
-                                        <Image
-                                            src={require("@/assets/images/logo/logo2.png")}
-                                            alt="Logo 2"
-                                        />
-                                    </div>
-                                </SwiperSlide>
-                                <SwiperSlide>
-                                    <div className="image">
-                                        <Image
-                                            src={require("@/assets/images/logo/logo3.png")}
-                                            alt="Logo 3"
-                                        />
-                                    </div>
-                                </SwiperSlide>
-                                <SwiperSlide>
-                                    <div className="image">
-                                        <Image
-                                            src={require("@/assets/images/logo/logo4.png")}
-                                            alt="Logo 4"
-                                        />
-                                    </div>
-                                </SwiperSlide>
-                                <SwiperSlide>
-                                    <div className="image">
-                                        <Image
-                                            src={require("@/assets/images/logo/logo5.png")}
-                                            alt="Logo 5"
-                                        />
-                                    </div>
-                                </SwiperSlide>
-                                <SwiperSlide>
-                                    <div className="image">
-                                        <Image
-                                            src={require("@/assets/images/logo/logo6.png")}
-                                            alt="Logo 6"
-                                        />
-                                    </div>
-                                </SwiperSlide>
-                            </Swiper>
-
-                            {/* Second Swiper Carousel */}
-                            <Swiper
-                                slidesPerView={2}
-                                loop={false}
-                                spaceBetween={30}
-                                breakpoints={{
-                                    0: {
-                                        slidesPerView: 2,
-                                        spaceBetween: 30,
-                                    },
-                                    400: {
-                                        slidesPerView: 2,
-                                        spaceBetween: 60,
-                                    },
-                                    600: {
-                                        slidesPerView: 3,
-                                        spaceBetween: 60,
-                                    },
-                                    750: {
-                                        slidesPerView: 4,
-                                        spaceBetween: 60,
-                                    },
-                                    991: {
-                                        slidesPerView: 5,
-                                        spaceBetween: 60,
-                                    },
-                                    1280: {
-                                        slidesPerView: 6,
-                                        spaceBetween: 60,
-                                    },
-                                }}
-                                className="swiper-container sl-parner"
-                            >
-                                <SwiperSlide>
-                                    <div className="image">
-                                        <Image
-                                            src={require("@/assets/images/logo/logo7.png")}
-                                            alt="Logo 7"
-                                        />
-                                    </div>
-                                </SwiperSlide>
-                                <SwiperSlide>
-                                    <div className="image">
-                                        <Image
-                                            src={require("@/assets/images/logo/logo8.png")}
-                                            alt="Logo 8"
-                                        />
-                                    </div>
-                                </SwiperSlide>
-                                <SwiperSlide>
-                                    <div className="image">
-                                        <Image
-                                            src={require("@/assets/images/logo/logo9.png")}
-                                            alt="Logo 9"
-                                        />
-                                    </div>
-                                </SwiperSlide>
-                                <SwiperSlide>
-                                    <div className="image">
-                                        <Image
-                                            src={require("@/assets/images/logo/logo10.png")}
-                                            alt="Logo 10"
-                                        />
-                                    </div>
-                                </SwiperSlide>
-                                <SwiperSlide>
-                                    <div className="image">
-                                        <Image
-                                            src={require("@/assets/images/logo/logo11.png")}
-                                            alt="Logo 11"
-                                        />
-                                    </div>
-                                </SwiperSlide>
-                                <SwiperSlide>
-                                    <div className="image">
-                                        <Image
-                                            src={require("@/assets/images/logo/logo12.png")}
-                                            alt="Logo 12"
-                                        />
-                                    </div>
-                                </SwiperSlide>
-                            </Swiper>
-                        </div>
+                    <div className="partners flex justify-center gap-[40px] flex-wrap items-center px-[30px]">
+                        {LOGOS.map((logo, index) => (
+                            <div className="item w-[100px]" key={index}>
+                                <Image
+                                    src={logo}
+                                    className="w-full object-contain"
+                                    alt="logo"
+                                />
+                            </div>
+                        ))}
                     </div>
                 </div>
             </div>
--- src/components/Token.tsx
+++ src/components/Token.tsx
@@ -8,7 +8,7 @@ export default function Token() {
         <section className="tf-section tf-item-details pb-mobie" id="token">
             <div className="container">
                 <div className="row">
-                    <div className="col-md-12">
+                    <div className="col-md-12 col-xl-6">
                         <div
                             className="tf-title st2 m-b50"
                             data-aos="fade-up"
@@ -17,32 +17,42 @@ export default function Token() {
                             <h4 className="title m-b17">Tokenomics</h4>
                             <p className="h8 sub-title">NANOEX Utility Token</p>
                         </div>
-                    </div>
-                    <div className="col-xl-6 col-lg-6 col-md-12">
-                        <div
-                            className="image-details"
-                            data-aos="fade-right"
-                            data-aos-duration="800"
-                        >
-                            <Image
-                                src={require("@/assets/images/common/nanoexchart.png")}
-                                alt=""
-                            />
+                        <div className="col-xl-12 col-lg-12 col-md-12">
+                            <div
+                                className="image-details"
+                                data-aos="fade-right"
+                                data-aos-duration="800"
+                            >
+                                <Image
+                                    className="m-b50"
+                                    src={require("@/assets/images/common/nanoexchart.png")}
+                                    alt=""
+                                />
 
-                            <a className="tf-button st2 btn-effect" href="#">
-                                <span className="boder-fade"></span>
-                                <span className="effect">View Whitepaper</span>
-                            </a>
+                                <a
+                                    className="tf-button st2 btn-effect"
+                                    href="#"
+                                >
+                                    <span className="boder-fade"></span>
+                                    <span className="effect">
+                                        View Whitepaper
+                                    </span>
+                                </a>
+                            </div>
                         </div>
                     </div>
-                    <div className="col-xl-6 col-lg-6 col-md-12">
+
+                    <div className="col-xl-6 col-lg-12 col-md-12">
                         <div
                             className="item-details"
                             data-aos="fade-left"
                             data-aos-duration="800"
                         >
-                            <h5>$nRWA</h5>
-                            <p className="sub">NANOEX UTILITY TOKEN</p>
+                            <h5>$nRWA Token</h5>
+                            <h6 className="sub">
+                                Powering the Future of Real-World Asset
+                                Tokenization
+                            </h6>
 
                             <p className="desc">
                                 Welcome to NanoEx RWA, the ultimate platform for
@@ -59,24 +69,31 @@ export default function Token() {
                                 infrastructure into fractionalized digital
                                 tokens.
                             </p>
-                            <Link href="#" className="tf-button btn-effect">
-                                <span className="boder-fade"></span>
-                                <svg
-                                    width="18"
-                                    height="18"
-                                    viewBox="0 0 18 18"
-                                    fill="none"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                >
-                                    <path
-                                        fillRule="evenodd"
-                                        clipRule="evenodd"
-                                        d="M12.8771 4.39102H13.0567C15.4181 4.39102 17.3346 6.26602 17.3346 8.56602V13.166C17.3346 15.466 15.4181 17.3327 13.0567 17.3327H4.94586C2.58446 17.3327 0.667969 15.466 0.667969 13.166V8.56602C0.667969 6.26602 2.58446 4.39102 4.94586 4.39102H5.12553C5.14264 3.39102 5.54477 2.45768 6.27201 1.75768C7.00781 1.04935 7.94894 0.691016 9.00986 0.666016C11.1317 0.666016 12.8514 2.33268 12.8771 4.39102ZM7.17038 2.64935C6.69125 3.11602 6.42602 3.73268 6.40891 4.39102H11.5937C11.5681 3.02435 10.4216 1.91602 9.00987 1.91602C8.35108 1.91602 7.66661 2.17435 7.17038 2.64935ZM12.2525 7.59935C12.6118 7.59935 12.8942 7.31601 12.8942 6.97435V6.00768C12.8942 5.66601 12.6118 5.38268 12.2525 5.38268C11.9017 5.38268 11.6108 5.66601 11.6108 6.00768V6.97435C11.6108 7.31601 11.9017 7.59935 12.2525 7.59935ZM6.31476 6.97435C6.31476 7.31601 6.03242 7.59935 5.67308 7.59935C5.32229 7.59935 5.0314 7.31601 5.0314 6.97435V6.00768C5.0314 5.66602 5.32229 5.38268 5.67308 5.38268C6.03242 5.38268 6.31476 5.66602 6.31476 6.00768V6.97435Z"
-                                        fill="white"
-                                    />
-                                </svg>
-                                <span className="effect">Buy $nRWA</span>
-                            </Link>
+                            <div className="flex gap-4">
+                                <a href="#" className="tf-button btn-effect">
+                                    <span className="boder-fade"></span>
+                                    <svg
+                                        width="14"
+                                        height="13"
+                                        viewBox="0 0 18 18"
+                                        fill="none"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                    >
+                                        <path
+                                            fillRule="evenodd"
+                                            clipRule="evenodd"
+                                            d="M12.8771 4.39102H13.0567C15.4181 4.39102 17.3346 6.26602 17.3346 8.56602V13.166C17.3346 15.466 15.4181 17.3327 13.0567 17.3327H4.94586C2.58446 17.3327 0.667969 15.466 0.667969 13.166V8.56602C0.667969 6.26602 2.58446 4.39102 4.94586 4.39102H5.12553C5.14264 3.39102 5.54477 2.45768 6.27201 1.75768C7.00781 1.04935 7.94894 0.691016 9.00986 0.666016C11.1317 0.666016 12.8514 2.33268 12.8771 4.39102ZM7.17038 2.64935C6.69125 3.11602 6.42602 3.73268 6.40891 4.39102H11.5937C11.5681 3.02435 10.4216 1.91602 9.00987 1.91602C8.35108 1.91602 7.66661 2.17435 7.17038 2.64935ZM12.2525 7.59935C12.6118 7.59935 12.8942 7.31601 12.8942 6.97435V6.00768C12.8942 5.66601 12.6118 5.38268 12.2525 5.38268C11.9017 5.38268 11.6108 5.66601 11.6108 6.00768V6.97435C11.6108 7.31601 11.9017 7.59935 12.2525 7.59935ZM6.31476 6.97435C6.31476 7.31601 6.03242 7.59935 5.67308 7.59935C5.32229 7.59935 5.0314 7.31601 5.0314 6.97435V6.00768C5.0314 5.66602 5.32229 5.38268 5.67308 5.38268C6.03242 5.38268 6.31476 5.66602 6.31476 6.00768V6.97435Z"
+                                            fill="#56c690"
+                                        />
+                                    </svg>
+                                    <span className="effect">Buy $nRWA</span>
+                                </a>
+                                <a href="#" className="tf-button btn-effect">
+                                    <span className="boder-fade"></span>
+
+                                    <span className="effect">How to Buy</span>
+                                </a>
+                            </div>
                             <div className="list-product">
                                 <div className="box corner-box">
                                     <p>Total Supply</p>
